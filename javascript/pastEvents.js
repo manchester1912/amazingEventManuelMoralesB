76c25797fb1c4f832d4664f3382056683cb8ae6b
manchester1912 <manuelbastidasba@gmail.com>
2024-05-13 01:08:11 -0300

    se rtealizaron ajustes de estilos y diseño de tarjetas

--- javascript/pastEvents.js
+++ javascript/pastEvents.js
@@ -217,15 +217,15 @@ let data = {
   
   function createdCard(cardsContainer, card) {
       let generateCard = document.createElement("div")
-      generateCard.classList.add("cards")
+      generateCard.classList.add("cards", "pt-2", "my-2", "mx-2", "text-white")
        
       generateCard.innerHTML = 
-      `<h2>${card.name}</h2>
-      <p>${card.description}</p>
-      <img src="${card.image}" alt="">`
+      `<img src="${card.image}" alt="">
+      <h2>${card.name}</h2>
+      <p>${card.description}</p>`
   
       let newChild = document.createElement("div")
-      newChild.classList.add("price", "fw-bold", "fs-5")
+      newChild.classList.add("price", "fw-bold", "fs-5", "pb-2")
       newChild.innerHTML = ` <p>Price: ${card.price} ${"USD"}</p>
       <a href="/details.html" class="btn btn-danger">Details</a>`
             
