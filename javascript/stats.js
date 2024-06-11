c951ae61c6b0f94c43f865b35063eecb2a41aecf
manchester1912 <manuelbastidasba@gmail.com>
2024-06-11 15:14:47 -0300

    se finaliza primera tabla de la pagina stats

--- javascript/stats.js
+++ javascript/stats.js
@@ -15,13 +15,22 @@
 
     let highestAssistance = document.getElementById("highest")
     let lowestAssistance = document.getElementById("lowest")
+    let largerCapacity = document.getElementById("capacity")
     let percentage = pastEvents.map(evento => ((evento.assistance / evento.capacity)* 100).toFixed(2))
+    let capacity = data.events.map(evento => evento.capacity)
+ 
    
-   
+      
     const maximumPercentage = Math.max(...percentage)
     const minimumPercentage = Math.min(...percentage)
+    const maximumCapacity = Math.max(...capacity)
+
+    let eventoMaxCapacity = data.events.find(evento => evento.capacity === maximumCapacity)
+  
     
-  highestAssistance.innerText = maximumPercentage
-  lowestAssistance.innerText = minimumPercentage
+  highestAssistance.innerText = maximumPercentage + " %"
+  lowestAssistance.innerText = minimumPercentage + " %"
+  largerCapacity.innerHTML = eventoMaxCapacity.category + ": "+ eventoMaxCapacity.name +" "+ maximumCapacity.toLocaleString() + " People"
+ 
  })
 
