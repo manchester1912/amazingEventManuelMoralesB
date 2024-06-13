314b5a4bbcc679aefad67c8b81c9bc77994f6372
manchester1912 <manuelbastidasba@gmail.com>
2024-06-13 00:19:01 -0300

    se corriguieron formulas de datos de tabla stats

--- javascript/stats.js
+++ javascript/stats.js
@@ -19,7 +19,13 @@
     let percentage = pastEvents.map(evento => ((evento.assistance / evento.capacity)* 100).toFixed(2))
     let capacity = data.events.map(evento => evento.capacity)
  
-   
+  //   let eventosConPorcentaje = pastEvents.map(evento => {
+  //     return {
+  //         porcentaje: ((evento.assistance / evento.capacity) * 100).toFixed(2),
+  //         categoria: evento.name
+  //     }
+  // })
+  // console.log(eventosConPorcentaje);
       
     const maximumPercentage = Math.max(...percentage)
     const minimumPercentage = Math.min(...percentage)
@@ -34,105 +40,93 @@
  
  })
 
- fetch(linkAmazing)
+
+fetch(linkAmazing)
  .then(responsive => responsive.json())
  .then(data2 => {
 
-  function sendData (item){
-    let fatherDiv = document.getElementById("fatherTable2")
-    let tableData = document.createElement("tr")
-    tableData.classList.add("text-center")
-  
-    tableData.innerHTML = `
-      <td>${item.category}</td>
-      <td>$ ${(item.price * item.assistance).toLocaleString()}</td>
-      <td>${((item.assistance / item.capacity) * 100).toFixed(2)} %</td>`
-  
-      fatherDiv.appendChild(tableData)
-   }
-  
-   function printData (array){
-    for (let index = 0; index < array.length; index++) {
-      sendData(array[index])
-      
-    }
-   }
-  
+    let pastEvents = data2.events.filter(event => data2.currentDate > event.date)
 
-  let pastEvents = []
-
-    for (let index = 0; index < data2.events.length; index++) {
-      if (data2.currentDate > data2.events[index].date) {
-         pastEvents.push(data2.events[index])
-      }
-    }
-  let newArray = pastEvents.map(event => ({category: event.category, assistance: event.assistance, price: event.price, capacity: event.capacity}))
-
-  let filterCategory = newArray.reduce((added, event) => {
-    if (!added[event.category]) {
-        added[event.category] = { category: event.category, assistance: 0, price: 0, capacity: 0 }
-    }
-    added[event.category].assistance += event.assistance
-    added[event.category].price += event.price
-    added[event.category].capacity += event.capacity
-    return added
-}, {})
-
-let arrayCategory = Object.keys(filterCategory).map(key => filterCategory[key])
-
-printData(arrayCategory)
-
-
- })
-
- fetch(linkAmazing)
- .then(responsive => responsive.json())
- .then(data2 => {
-
-  function sendData (item){
-    let fatherDiv2 = document.getElementById("fatherTable")
-    let tableData2 = document.createElement("tr")
-    tableData2.classList.add("text-center")
-  
-    tableData2.innerHTML = `
-      <td>${item.category}</td>
-      <td>$ ${(item.price * item.estimate).toLocaleString()}</td>
-      <td>${((item.estimate / item.capacity) * 100).toFixed(2)} %</td>`
-  
-      fatherDiv2.appendChild(tableData2)
-   }
-  
-   function printData (array){
-    for (let index = 0; index < array.length; index++) {
-      sendData(array[index])
-      
-    }
-   }
-  
-
-   let upcomingEv = []
-
-    for (let index = 0; index < data2.events.length; index++) {
-        if (data2.currentDate < data2.events[index].date) {
-            upcomingEv.push(data2.events[index])
+     let filterCategory = pastEvents.reduce((added, event) => {
+        if (!added[event.category]) {
+            added[event.category] = { category: event.category, total: 0, capacity: 0, assistance: 0 }
         }
+      
+        added[event.category].total += event.assistance * event.price
+        added[event.category].capacity += event.capacity
+        added[event.category].assistance += event.assistance
+        return added
+    }, {})
+
+    let arrayCategory2 = Object.values(filterCategory)
+
+    function sendData(item) {
+        let fatherDiv = document.getElementById("fatherTable2")
+        let tableData = document.createElement("tr")
+        tableData.classList.add("text-center")
+  
+        tableData.innerHTML = `
+            <td>${item.category}</td>
+            <td>$ ${(item.total).toLocaleString()}</td>
+            <td>${((item.assistance / item.capacity) * 100).toFixed(2)} %</td>`
+    
+        fatherDiv.appendChild(tableData)
+      }
+   
+    function printData(array) {
+       for (let index = 0; index < array.length; index++) {
+        sendData(array[index])
+        
+       }
     }
-  let newArray = upcomingEv.map(event => ({category: event.category, estimate: event.estimate, price: event.price, capacity: event.capacity}))
+   
+    printData(arrayCategory2)
 
-  let filterCategory = newArray.reduce((added, event) => {
-    if (!added[event.category]) {
-        added[event.category] = { category: event.category, estimate: 0, price: 0, capacity: 0 }
+  })
+
+
+fetch(linkAmazing)
+ .then(responsive => responsive.json())
+ .then(data => {
+
+    let upcomingEv = data.events.filter(event => data.currentDate < event.date)
+
+     let filterCategory = upcomingEv.reduce((added, event) => {
+        if (!added[event.category]) {
+            added[event.category] = { category: event.category, total: 0, capacity: 0, estimate: 0 }
+        }
+      
+        added[event.category].total += event.estimate * event.price
+        added[event.category].capacity += event.capacity
+        added[event.category].estimate += event.estimate
+        return added
+    }, {})
+
+    let arrayCategory = Object.values(filterCategory)
+
+    function sendData(item) {
+        let fatherDiv2 = document.getElementById("fatherTable")
+        let tableData2 = document.createElement("tr")
+        tableData2.classList.add("text-center")
+  
+        tableData2.innerHTML = `
+            <td>${item.category}</td>
+            <td>$ ${(item.total).toLocaleString()}</td>
+            <td>${((item.estimate / item.capacity) * 100).toFixed(2)} %</td>`
+    
+        fatherDiv2.appendChild(tableData2)
+      }
+   
+    function printData(array) {
+       for (let index = 0; index < array.length; index++) {
+        sendData(array[index])
+        
+       }
     }
-    added[event.category].estimate += event.estimate
-    added[event.category].price += event.price
-    added[event.category].capacity += event.capacity
-    return added
-}, {})
+   
+    printData(arrayCategory)
 
-let arrayCategory = Object.keys(filterCategory).map(key => filterCategory[key])
-
-printData(arrayCategory)
- })
+  })
 
 
 
